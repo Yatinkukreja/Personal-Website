b2d10ddccac70e223f649584f189a0a7f28b3482
Yatin Kukreja <ykukreja@purdue.edu>
2024-08-13 11:26:34 -0700

    added resume w GPA

--- frontend/src/components/Resume.tsx
+++ frontend/src/components/Resume.tsx
@@ -6,6 +6,8 @@ const Resume: FC = () => {
       {/* Embedding the PDF within an iframe */}
       {/* <img src="/logo192.png"></img> */}
       {/*when changing, must update public folder as well */}
+      {/*Assuming resume is in downloads folder: */}
+      {/*cp C:\Users\Yatin\Downloads\{RESUME_NAME}.pdf C:\Users\Yatin\OneDrive\Documents\GitHub\Personal-Website\frontend\public */}
       <iframe
         src="/Resume_Yatin_Kukreja_August_2024.pdf"
         style={{ width: '100%', height: '87vh'}}
